365f737b08db3a65f4b8383bcecda8703dc23baa
Samantha <samanthavillafana8@gmail.com>
2024-02-07 18:27:00 -0500

    render shows to the dom

--- script.js
+++ script.js
@@ -7,7 +7,26 @@ function fetchShows(){
         const searchInput = document.getElementById('search').value;
         fetch(`https://api.tvmaze.com/search/shows?q=${searchInput}`)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(shows => renderShows(shows))
+    })
+}
+
+//
+
+function renderShows(shows){
+    const showCollection = document.getElementById('show-collection');
+
+    shows.forEach(obj => {
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.innerHTML = `
+        <h2>${obj.show.name}</h2>
+        <h4>${obj.show.network.name}</h4>
+        <h4>${obj.show.genres}</h4>
+        <h4>${obj.show.rating.average}</h4>
+      
+        `;
+        showCollection.appendChild(card);
     })
 }
 
