40cd844b36b7e3ad1acc25bbf07e8264fc020542
Samantha <samanthavillafana8@gmail.com>
2024-02-12 17:16:16 -0500

    add borderhighlight event listener

--- script.js
+++ script.js
@@ -10,25 +10,53 @@ function fetchShows(){
         .then(shows => renderShows(shows))
     })
 }
+//Add Highlight to Card
 
-//
+function addHighlight(card) {
+    card.style.borderColor = 'pink';
+}
+
+function removeHighlight(card) {
+    card.style.borderColor = 'black';
+}
+
+//Render Shows
 
 function renderShows(shows){
     const showCollection = document.getElementById('show-collection');
-
     shows.forEach(obj => {
         const card = document.createElement('div');
-        card.className = 'card';
+        card.className = 'card'; 
         card.innerHTML = `
+        <img class= "card-img"src="${obj.show.image.original}" alt=""/>
         <h2>${obj.show.name}</h2>
-        <h4>${obj.show.network.name}</h4>
-        <h4>${obj.show.genres}</h4>
-        <h4>${obj.show.rating.average}</h4>
-      
+        <h4>Network: ${obj.show.network.name}</h4>
+        <h4>Genre: ${obj.show.genres}</h4>
+        <h4>Rating: ${obj.show.rating.average}</h4>
+        <button class="summary-btn">Summary ⌄</button>
         `;
         showCollection.appendChild(card);
+        const summaryButton = card.querySelector('.summary-btn');
+        summaryButton.addEventListener('click', () => {
+            const summaryContainer = document.createElement('div');
+            summaryContainer.className = 'summary-container'
+            summaryContainer.innerHTML = `
+            <p>${obj.show.summary}</p>
+            `;
+            card.appendChild(summaryContainer)
+            
+        })
+        card.addEventListener('mouseover', () => {
+            addHighlight(card);
+        });
+
+        card.addEventListener('mouseout', () => {
+            removeHighlight(card);
+        });
+
     })
 }
+
 
 //Invoking my function
 fetchShows()
